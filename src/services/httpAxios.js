baed286c2ae6cf9b15b9af45066e9b3f99dc02ca
Mo Amir <130815672+mo-amir-code@users.noreply.github.com>
2024-03-24 02:46:34 +0530

    Update httpAxios.js

--- src/services/httpAxios.js
+++ src/services/httpAxios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const httpAxios = axios.create({
-  baseURL: import.meta.env.VITE_SERVER_ORIGIN,
+  baseURL: "https://instagram-fullstack-amir.vercel.app",
   // baseURL: "http://localhost:8000",
 });
