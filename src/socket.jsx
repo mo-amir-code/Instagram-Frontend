217679b112298af82e6444532433085d7269a53a
Mo Amir <mo.amir.code@gmail.com>
2024-03-24 23:52:24 +0530

    socket.jsx

--- src/socket.jsx
+++ src/socket.jsx
@@ -4,9 +4,9 @@ const SOCKET_ENDPOINT = import.meta.env.VITE_SERVER_ORIGIN;
 let socket;
 
 const connectSocket = (userId) => {
-  socket = io(SOCKET_ENDPOINT, { query: `userId=${userId}`});
-
-  console.log("connection");
+  socket = io(SOCKET_ENDPOINT, { query: `userId=${userId}` })
+    .then(() => console.log("connection"))
+    .catch((error) => console.log(error));
 
   return () => {
     socket.disconnect(userId);
